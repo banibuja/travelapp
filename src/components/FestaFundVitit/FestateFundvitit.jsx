5e01f26cddca4107c489d4dd289dfbea557f29a9
ArionR8 <149816094+ArionR8@users.noreply.github.com>
2024-12-13 19:59:31 +0100

    update

--- src/components/FestaFundVitit/FestateFundvitit.jsx
+++ src/components/FestaFundVitit/FestateFundvitit.jsx
@@ -4,98 +4,112 @@ import Footer from '../layout/Footer';
 function FestateFundvitit() {
   return (
     <div>
+      <div className="max-w-7xl m-auto">
+        <div className="w-full h-80 flex flex-col items-center my-9">
+          <div className="w-full text-center">
+            <h1 className="text-3xl py-5 font-mono">Festat e Fundvitit</h1>
+          </div>
+          <div className="w-[48rem] font-medium text-[#555555] text-[20px] mt-[3rem]">
+            Festat e fundvitit sjellin një atmosferë magjike dhe gëzim të pashoq për njerëzit në mbarë botën. 
+            Dritat shumëngjyrëshe, fishekzjarrët dhe traditat unike i bëjnë këto ditë të paharrueshme. 
+            Për ta bërë këtë periudhë edhe më të veçantë, <span className="font-semibold">Bani Travel</span> ju ndihmon të zgjidhni destinacionet ideale për të vizituar. 
+            Nga madhështia e <span className="font-semibold">New York-ut</span> në SHBA, romantizmi i <span className="font-semibold">Parisit</span> në Evropë, deri te moderniteti i <span className="font-semibold">Dubait</span> në Azi, 
+            këto tre qytete kryesore ofrojnë eksperienca të paharrueshme për të festuar fundvitin në mënyrën më të bukur.
+          </div>
+        </div>
+
       <div className="slider-container">
-        <div className="slider">
-          <h2 className="text-center text-2xl font-bold mb-4">US</h2>
-          <div className="flex justify-center space-x-4">
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://media.timeout.com/images/105730957/750/422/image.jpg"
-                alt="New York"
-              />
-              <p className="font-semibold mt-2">New York</p>
-            </div>
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://ktla.com/wp-content/uploads/sites/4/2020/12/AP20365030764895.jpg?strip=1"
-                alt="Las Vegas"
-              />
-              <p className="font-semibold mt-2">Las Vegas</p>
-            </div>
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://miro.medium.com/v2/resize:fit:1200/1*CGvTjTOr6_h40pllQA2ATw.png"
-                alt="San Francisco"
-              />
-              <p className="font-semibold mt-2">San Francisco</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="slider mt-12">
-          <h2 className="text-center text-2xl font-bold mb-4">EU</h2>
-          <div className="flex justify-center space-x-4">
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://www.bvjhostelparis.com/wp-content/uploads/2017/07/FIRE-WORKS-PARIS-14-JULY.jpg"
-                alt="Paris"
-              />
-              <p className="font-semibold mt-2">Paris</p>
-            </div>
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://www.wien.info/resource/image/309314/19x10/1200/630/6e921a41966756ab2b894918a4fb557e/B78CB8E009BF476058916BD62BC65D8F/silvester-silvesterpfad-graben.jpg"
-                alt="Vienna"
-              />
-              <p className="font-semibold mt-2">Vienna</p>
-            </div>
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://cdn-imgix.headout.com/microbrands-content-image/image/505c0f8d8d05ad0ddc6fb6da463ca924-Budapest%201.jpg?auto=format&w=1222.3999999999999&h=687.6&q=90&fit=crop&ar=16%3A9&crop=faceshttps://cdn-imgix.headout.com/microbrands-content-image/image/505c0f8d8d05ad0ddc6fb6da463ca924-Budapest%201.jpg?auto=format&w=1222.3999999999999&h=687.6&q=90&fit=crop&ar=16%3A9&crop=faces"
-                alt="Budapest"
-              />
-              <p className="font-semibold mt-2">Budapest</p>
+          <div className="slider">
+            <h2 className="text-center text-2xl font-bold mb-4">US</h2>
+            <div className="flex justify-center space-x-4">
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://media.timeout.com/images/105730957/750/422/image.jpg"
+                  alt="New York"
+                />
+                <p className="font-semibold mt-2">New York</p>
+              </a>
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://ktla.com/wp-content/uploads/sites/4/2020/12/AP20365030764895.jpg?strip=1"
+                  alt="Las Vegas"
+                />
+                <p className="font-semibold mt-2">Las Vegas</p>
+              </a>
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://miro.medium.com/v2/resize:fit:1200/1*CGvTjTOr6_h40pllQA2ATw.png"
+                  alt="San Francisco"
+                />
+                <p className="font-semibold mt-2">San Francisco</p>
+              </a>
             </div>
           </div>
-        </div>
 
-        <div className="slider mt-12">
-          <h2 className="text-center text-2xl font-bold mb-4">Asia</h2>
-          <div className="flex justify-center space-x-4">
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Dubai_Skyline.jpg/1200px-Dubai_Skyline.jpg"
-                alt="Dubai"
-              />
-              <p className="font-semibold mt-2">Dubai</p>
+          <div className="slider mt-12">
+            <h2 className="text-center text-2xl font-bold mb-4">EU</h2>
+            <div className="flex justify-center space-x-4">
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://www.bvjhostelparis.com/wp-content/uploads/2017/07/FIRE-WORKS-PARIS-14-JULY.jpg"
+                  alt="Paris"
+                />
+                <p className="font-semibold mt-2">Paris</p>
+              </a>
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://www.wien.info/resource/image/309314/19x10/1200/630/6e921a41966756ab2b894918a4fb557e/B78CB8E009BF476058916BD62BC65D8F/silvester-silvesterpfad-graben.jpg"
+                  alt="Vienna"
+                />
+                <p className="font-semibold mt-2">Vienna</p>
+              </a>
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://cdn-imgix.headout.com/microbrands-content-image/image/505c0f8d8d05ad0ddc6fb6da463ca924-Budapest%201.jpg?auto=format&w=1222.3999999999999&h=687.6&q=90&fit=crop&ar=16%3A9&crop=faces"
+                  alt="Budapest"
+                />
+                <p className="font-semibold mt-2">Budapest</p>
+              </a>
             </div>
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Tokyo_Skyline.jpg/1200px-Tokyo_Skyline.jpg"
-                alt="Tokyo"
-              />
-              <p className="font-semibold mt-2">Tokyo</p>
-            </div>
-            <div className="country-card w-1/3 text-center">
-              <img
-                className="rounded-lg object-cover w-full h-auto"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Singapore_Skyline.jpg/1200px-Singapore_Skyline.jpg"
-                alt="Singapore"
-              />
-              <p className="font-semibold mt-2">Singapore</p>
+          </div>
+
+          <div className="slider mt-12">
+            <h2 className="text-center text-2xl font-bold mb-4">Asia</h2>
+            <div className="flex justify-center space-x-4">
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://c.regencyholidays.com/blog/blog/content/images/2021/11/New-Year-In-UAE-2022--2-.webp"
+                  alt="Dubai"
+                />
+                <p className="font-semibold mt-2">Dubai</p>
+              </a>
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://media.timeout.com/images/105692512/image.jpg"
+                  alt="Tokyo"
+                />
+                <p className="font-semibold mt-2">Tokyo</p>
+              </a>
+              <a href="#" className="country-card w-1/3 text-center transform transition duration-300 hover:scale-105 hover:shadow-lg">
+                <img
+                  className="rounded-lg object-cover w-full h-auto"
+                  src="https://ik.imagekit.io/tvlk/blog/2024/10/shutterstock_727168480.jpg?tr=q-70,w-625,dpr-2"
+                  alt="Singapore"
+                />
+                <p className="font-semibold mt-2">Singapore</p>
+              </a>
             </div>
           </div>
         </div>
       </div>
-
       <Footer />
     </div>
   );
--- src/components/Greqi/Greqi.jsx
+++ src/components/Greqi/Greqi.jsx
@@ -1,76 +1,84 @@
 import React from 'react';
 import Footer from '../layout/Footer';
+import Carousel from '../carousel.component';
 
-function Greqi() {
+const Greqi = () => {
+  let slides = [
+    "https://150429065.v2.pressablecdn.com/wp-content/uploads/2020/01/Santorini-Greece-1000x658.jpg", 
+    "https://greekcitytimes.com/wp-content/uploads/2018/07/http-cdn.cnn_.com-cnnnext-dam-assets-170606121035-greece-travel-destination-shutterstock-560829934.jpg", 
+    "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/c0/98/c5/caption.jpg", 
+  ];
+
   return (
-    <div>
-      <div className="p-10">
-        <div className="flex justify-center items-center space-x-4 mx-auto truncate whitespace-break-spaces text-navy text-xl font-bold tracking-tight lg:text-2xl">
-          <span>Greqi Home | Paket Arazhman</span>
+    <>
+      <div className="overflow-x-auto max-w-4xl m-auto">
+        <div className="w-full text-center mb-14 my-9">
+          <h1 className="text-3xl py-5 font-mono">Visit Greece</h1>
         </div>
 
-        <div className="flex justify-center items-center space-x-4 mt-8">
-          <div className="card1 w-1/4 text-center relative">
-            <img
-              className="rounded-lg object-cover w-full h-auto transition-transform duration-300 ease-in-out transform hover:scale-105"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Acropolis_of_Athens_%28pixinn.net%29.jpg/1200px-Acropolis_of_Athens_%28pixinn.net%29.jpg"
-              alt="Athens"
-            />
-            <p className="city font-semibold text-white absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 px-3 py-1 rounded-lg">Athens</p>
-          </div>
+        <Carousel slides={slides} />
+      </div>
 
-          <div className="card1 w-1/4 text-center relative">
-            <img
-              className="rounded-lg object-cover w-full h-auto transition-transform duration-300 ease-in-out transform hover:scale-105"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Port_of_Piraeus.jpg/1200px-Port_of_Piraeus.jpg"
-              alt="Piraeus"
-            />
-            <p className="city font-semibold text-white absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 px-3 py-1 rounded-lg">Piraeus</p>
-          </div>
-
-          <div className="card1 w-1/4 text-center relative">
-            <img
-              className="rounded-lg object-cover w-full h-auto transition-transform duration-300 ease-in-out transform hover:scale-105"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lamia_castle.JPG/1200px-Lamia_castle.JPG"
-              alt="Lamia"
-            />
-            <p className="city font-semibold text-white absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 px-3 py-1 rounded-lg">Lamia</p>
-          </div>
+      <div className="bg-white text-gray-800 font-sans mt-16">
+        <div className="bg-blue-500 text-white py-4 px-6">
+          <h1 className="text-xl max-w-4xl mx-auto font-bold">Santorini - A Piece of Paradise</h1>
         </div>
 
-        <div className="flex justify-center items-center space-x-4 mt-8">
-          <div className="card1 w-1/4 text-center relative">
-            <img
-              className="rounded-lg object-cover w-full h-auto transition-transform duration-300 ease-in-out transform hover:scale-105"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Fira_Santorini.jpg/1200px-Fira_Santorini.jpg"
-              alt="Fira"
-            />
-            <p className="city font-semibold text-white absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 px-3 py-1 rounded-lg">Fira</p>
-          </div>
+        <div className="max-w-4xl mx-auto p-6">
+          <div className="flex flex-wrap">
+            <div className="w-full md:w-1/2 pr-4">
+              <div className="flex flex-col items-start">
+                <p className="px-8 my-auto h-fit text-left text-blue-800 max-w-xs">
+                  Athina është një qytet që bashkon historinë dhe modernitetin. Akropoli dhe Partenoni janë atraksionet kryesore, ndërsa lagjet e vjetra dhe plazhet përreth ofrojnë një përvojë të pasur kulturore dhe natyrore.
+                </p>
+                <a
+                  href=""
+                  className="bg-blue-500 text-white py-2 px-4 rounded-md mt-4 text-center hover:bg-blue-600"
+                >
+                  Ofertat për Athin
+                </a>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 pr-4">
+              <img
+                src="https://expertvagabond.com/wp-content/uploads/things-to-do-athens-guide.jpg"
+                alt="athen"
+                className="h-64 w-auto ml-auto rounded-lg shadow-lg"
+              />
+            </div>
 
-          <div className="card1 w-1/4 text-center relative">
-            <img
-              className="rounded-lg object-cover w-full h-auto transition-transform duration-300 ease-in-out transform hover:scale-105"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Santorini_island.jpg/1200px-Santorini_island.jpg"
-              alt="Santorini"
-            />
-            <p className="city font-semibold text-white absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 px-3 py-1 rounded-lg">Santorini</p>
-          </div>
+            <div className="flex">
+              <img
+                src="https://www.greece-is.com/wp-content/uploads/2018/03/santorini-2.jpg"
+                alt="Santorini Sunset"
+                className="h-96 ml-auto rounded-lg shadow-lg"
+              />
+              <p className="px-36 my-auto h-fit text-center text-blue-800">
+                The island's famous sunsets are a must-see experience. As the sun sets over the caldera, the view is breathtaking and unforgettable.
+              </p>
+            </div>
 
-          <div className="card1 w-1/4 text-center relative">
-            <img
-              className="rounded-lg object-cover w-full h-auto transition-transform duration-300 ease-in-out transform hover:scale-105"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Chania_venetian_port.jpg/1200px-Chania_venetian_port.jpg"
-              alt="Chania"
-            />
-            <p className="city font-semibold text-white absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 px-3 py-1 rounded-lg">Chania</p>
+            <div className="flex">
+              <p className="px-36 my-auto h-fit text-center text-blue-800">
+                Santorini is also home to ancient ruins and archaeological sites, including the famous Akrotiri, which was destroyed by a volcanic eruption in the 16th century BC.
+              </p>
+              <img
+                src="https://media.greece-is.com/wp-content/uploads/2018/02/akrotiri-santorini.jpg"
+                alt="Akrotiri Archaeological Site"
+                className="h-96 ml-auto rounded-lg shadow-lg"
+              />
+            </div>
           </div>
         </div>
       </div>
 
+      <footer className="text-center text-sm text-gray-600 mt-10">
+        <p>Discover Greece</p>
+      </footer>
+
       <Footer />
-    </div>
+    </>
   );
-}
+};
 
 export default Greqi;
